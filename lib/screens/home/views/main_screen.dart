f8da45cd4d4549bcdce4c8ecde3e1c1696963a1b
KaungMinKhant <kaungminkhant.edka@gmail.com>
2024-02-09 13:27:46 +0630

    first blokc

--- lib/screens/home/views/main_screen.dart
+++ lib/screens/home/views/main_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -25,40 +27,51 @@ class MainView extends StatelessWidget {
                       SizedBox(
                         width: 8,
                       ),
-                      Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                        Text(
-                          "Welcome",
-                          style: TextStyle(
-                            color: Colors.grey,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 12,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 3,
-                        ),
-                        Text(
-                          "Kaung Min Khant",
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 15,
-                          ),
-                        )
-                      ]),
+                      Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Welcome",
+                              style: TextStyle(
+                                color: Colors.grey,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 12,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 3,
+                            ),
+                            Text(
+                              "Kaung Min Khant",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                              ),
+                            )
+                          ]),
                     ],
                   ),
-                IconButton(
-                  onPressed: () {},
-                  icon:const Icon(CupertinoIcons.settings)
-                  ),
-
+                  IconButton(
+                      onPressed: () {},
+                      icon: const Icon(CupertinoIcons.settings)),
                 ],
               ),
             ),
-
-            
-          
+            const SizedBox(height: 10),
+            Container(
+              height: MediaQuery.of(context).size.width / 2,
+              decoration: BoxDecoration(
+                  color: Colors.red,
+                  gradient: const LinearGradient(colors: [
+                    Color(0xFFFF8D6C),
+                    Color(0xFFE064F7),
+                    Color(0xFF00B2E7),
+                  ],
+                  transform:  GradientRotation(pi/4),
+                  ),
+                  borderRadius: BorderRadius.circular(25)),
+            )
           ],
         ),
       ),
